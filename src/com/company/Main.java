26ba33e2f39e76862eb9c5192dfbbf5daab2c95d
Ronald Escobar <ronaldescobarj@gmail.com>
2021-01-12 19:45:33 -0400

    added sum

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
         int myInteger = 3;
+        int a = 2;
+        int b = 3;
+        int result = a + b;
 	// write your code here
     }
 }
