96c30c88ac30fa1b502dd9333dae459953044055
Ronald Escobar <ronaldescobarj@gmail.com>
2021-01-14 17:23:22 -0400

    created instance of myMath on main. modified myMath methods to not be static

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,16 +5,17 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
+        MyMath myMath = new MyMath();
         int myInteger = 3;
         int a = 9;
         int b = 2;
-        int result = MyMath.sum(a, b);
+        int result = myMath.sum(a, b);
         int x = 2;
         int y = 4;
         int product = multiply(x, y);
         int n1 = 5;
         int n2 = 4;
-        int substraction = MyMath.substract(n1, n2);
+        int substraction = myMath.substract(n1, n2);
 	// write your code here
     }
 
--- src/com/company/MyMath.java
+++ src/com/company/MyMath.java
@@ -1,11 +1,11 @@
 package com.company;
 
 public class MyMath {
-  public static int substract(int n1, int n2) {
+  public int substract(int n1, int n2) {
     return n1 - n2;
   }
 
-  public static int sum(int a, int b) {
+  public int sum(int a, int b) {
     return a + b;
   }
 }
