0f30eb0e4b9051995ecea177dd28dbb815a1370f
Ronald Escobar <ronaldescobarj@gmail.com>
2021-01-13 21:44:09 -0400

    updated var b

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         int myInteger = 3;
         int a = 5;
-        int b = 5;
+        int b = 2;
         int result = sum(a, b);
 	// write your code here
     }
