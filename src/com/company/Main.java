ac2f939b802daf3d0c57f7a657379168181bafa6
Ronald Escobar <ronaldescobarj@gmail.com>
2021-01-14 16:54:55 -0400

    moved sum to my math

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,7 @@ public class Main {
         int myInteger = 3;
         int a = 9;
         int b = 2;
-        int result = sum(a, b);
+        int result = MyMath.sum(a, b);
         int x = 2;
         int y = 4;
         int product = multiply(x, y);
@@ -16,10 +16,6 @@ public class Main {
         int n2 = 4;
         int substraction = MyMath.substract(n1, n2);
 	// write your code here
-    }
-
-    public static int sum(int a, int b) {
-        return a + b;
     }
 
     public static int multiply(int a, int b) {
--- src/com/company/MyMath.java
+++ src/com/company/MyMath.java
@@ -4,4 +4,8 @@ public class MyMath {
   public static int substract(int n1, int n2) {
     return n1 - n2;
   }
+
+  public static int sum(int a, int b) {
+    return a + b;
+  }
 }
