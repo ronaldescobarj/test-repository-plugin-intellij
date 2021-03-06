7c5b3d65c0588d3ef5e398c5d34630d2426d1fc2
ronaldescobarj <ronaldescobarj@gmail.com>
2021-03-06 17:23:59 -0400

    added two classes

---
+++ src/com/company/Bicycle.java
@@ -0,0 +1,12 @@
+package com.company;
+public class Bicycle {
+
+  public String getSpeed() {
+    String bicycleSpeed = "Bicycle speed";
+    return bicycleSpeed;
+  }
+
+  public void pedal() {
+    System.out.println("now pedaling");
+  }
+}
---
+++ src/com/company/Car.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Car {
+  public String getSpeed() {
+    return "Car speed";
+  }
+
+  public void checkGps() {
+    System.out.println("gps works");
+  }
+}
