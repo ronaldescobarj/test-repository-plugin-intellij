b8157ebfcdd5eefe4644e800edb880ccd6882363
ronaldescobarj <ronaldescobarj@gmail.com>
2021-03-10 23:50:18 -0400

    prepared for push down attribute

--- src/com/company/Device.java
+++ src/com/company/Device.java
@@ -3,6 +3,7 @@ package com.company;
 public abstract class Device {
     private String brand;
     protected int battery;
+    protected boolean isAndroid;
 
     public void format() {
         System.out.println("formatting");
---
+++ src/com/company/SmartWatch.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class SmartWatch extends Device {
+    public void shutDown() {
+        System.out.println("smart watch shut down");
+    }
+}
