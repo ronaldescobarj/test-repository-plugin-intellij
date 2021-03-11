f7966cb0c53b85de021b340e27a550f26eeb1678
ronaldescobarj <ronaldescobarj@gmail.com>
2021-03-11 00:27:56 -0400

    renamed attribute?

--- src/com/company/Phone.java
+++ src/com/company/Phone.java
@@ -1,7 +1,7 @@
 package com.company;
 
 public class Phone extends Device {
-    private String simCard;
+    private String simCardData;
     private boolean isAndroid;
 
     public void shutDown() {
