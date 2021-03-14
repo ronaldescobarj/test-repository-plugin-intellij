e20c95f783bf34a52d80a8daf91f3be469e482ae
ronaldescobarj <ronaldescobarj@gmail.com>
2021-03-14 01:23:28 -0400

    a

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,11 +16,14 @@ public class Main {
       return userId == 1 ? "Menu1" : "Menu2";
     }
 
-    public static void showGoodbyeMessage() {
-      System.out.println("Goodbye");
+    public static void showGoodbyeMessage(String name) {
+      System.out.println("Goodbye!");
+      System.out.println("Have a great day");
+      System.out.println(name);
+      System.out.println("I hope to see you soon");
     }
 
-    public static String showUserData(String name, String country, int age) {
+    public static String showUserData(String name, int age, String country) {
       return "Name: " + name + "Country: " + country + "Age" + age;
     }
 
