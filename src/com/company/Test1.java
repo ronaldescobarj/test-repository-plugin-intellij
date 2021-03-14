6629ad2d8f98b2f6dbdffd393d3712c709cd63c3
ronaldescobarj <ronaldescobarj@gmail.com>
2021-03-13 23:15:06 -0400

    extracted interface?

--- src/com/company/Test1.java
+++ src/com/company/Test1.java
@@ -1,7 +1,8 @@
 package com.company;
 
-public class Test1 {
+public class Test1 implements TestInterface {
 
+    @Override
     public void xd() {
         System.out.println("xd");
     }
--- src/com/company/Test2.java
+++ src/com/company/Test2.java
@@ -1,6 +1,6 @@
 package com.company;
 
-public class Test2 {
+public class Test2 implements TestInterface {
 
     public void xd() {
         System.out.println("xd");
