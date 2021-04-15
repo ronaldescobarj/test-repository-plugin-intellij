56381d6e264fd30d3473b14afe7561b45ab54674
ronaldescobarj <ronaldescobarj@gmail.com>
2021-04-14 20:26:05 -0400

    t8

--- src/com/company/Bicycle.java
+++ src/com/company/Bicycle.java
@@ -17,6 +17,7 @@ public class Bicycle extends Vehicle {
     System.out.println("test5");
     System.out.println("test6");
     System.out.println("test7");
+    System.out.println("test8");
   }
 
   public void test(String str1, String str2) {
