c3629cf0f73f76535df7583695f59143a68d5022
ronaldescobarj <ronaldescobarj@gmail.com>
2021-04-28 22:13:29 -0400

    a

--- src/com/company/Testing.java
+++ src/com/company/Testing.java
@@ -1,5 +1,28 @@
 package com.company;
 
-public interface Animal {
-    private int x = 5;
+public class TestClass extends SuperClass {
+
+  private String pepito = "pepito";
+  
+  public TestClass()
+  {
+    int var = 123;
+    int toBeDeleted = 5667;
+  }
+
+  private void lol()
+  {
+    int x = 2;
+    String y = "y";
+    System.out.println("hi");
+  }
+
+  private String getPepito() {
+    String pepito = "PEPITO";
+    int aaaaa = 2;
+    System.out.println(aaaaa);
+    return pepito.toLowerCase();
+  }
+
+  private void newMethod()  { return; }
 }
